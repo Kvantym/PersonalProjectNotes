4afb83836354ff2046ff564d6004130a9c43a355
Kvantym <144225547+Kvantym@users.noreply.github.com>
2026-01-15 12:49:17 +0200

    Remove Guid to string converter for entity properties
    Removed the Guid to string value converter setup for entity properties.

--- PersonalProjectNotes.Data/AppDbContext.cs
+++ PersonalProjectNotes.Data/AppDbContext.cs
@@ -78,24 +78,16 @@ namespace PersonalProjectNotes.Data
                 .IsRequired()
                 .OnDelete(DeleteBehavior.Cascade); // каскадне видалення
 
-                // 1. Створюємо конвертер: Guid (C#) <-> string (MySQL)
-var guidConverter = new Microsoft.EntityFrameworkCore.Storage.ValueConversion.ValueConverter<Guid, string>(
-    v => v.ToString().ToLower(), // В базу йде рядок у нижньому регістрі
-    v => Guid.Parse(v)           // З бази рядок парситься назад у Guid
-);
-
-// 2. Проходимо по всіх сутностях і налаштовуємо кожен Guid
-foreach (var entityType in builder.Model.GetEntityTypes())
-{
-    foreach (var property in entityType.GetProperties())
+ foreach (var entityType in builder.Model.GetEntityTypes())
     {
-        if (property.ClrType == typeof(Guid) || property.ClrType == typeof(Guid?))
+        foreach (var property in entityType.GetProperties())
         {
-            property.SetValueConverter(guidConverter); // Прив'язуємо конвертер
-            property.SetColumnType("char(36)");         // Встановлюємо тип колонки
+            if (property.ClrType == typeof(Guid) || property.ClrType == typeof(Guid?))
+            {
+                property.SetColumnType("char(36)");
+            }
         }
     }
-}
         }
     }
 }
