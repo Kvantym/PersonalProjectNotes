5e668a5b99f07dfc3f244a40222027299f24fefa
Kvantym <144225547+Kvantym@users.noreply.github.com>
2026-01-15 13:48:29 +0200

    Update Board.cs

--- PersonalProjectNotes.Domain/Entities/Board.cs
+++ PersonalProjectNotes.Domain/Entities/Board.cs
@@ -10,6 +10,7 @@ namespace PersonalProjectNotes.Domain.Entities
         public DateTime UpdatedAt { get; set; }
         public Guid UserId { get; set; }
         public ApplicationUser User { get; set; }
+        [NotMapped]
         public UserAction Action { get; set; }
         public ICollection<ListCart> ListCart { get; set; } = new List<ListCart>();
         public ICollection<ActivityBoard> ActivityBoards { get; set; } = new List<ActivityBoard>();
